3e5abc31e5e306fd996ffe8e21d87e3a48dae9e9
Ilqar Ilyasov <ilqar_ilyasov@yahoo.com>
2019-03-04 14:52:52 -0600

    Create a protocol and extend URLSession

---
+++ Astronomy/Networking/NetworkDataLoader.swift
@@ -0,0 +1,27 @@
+//
+//  NetworkDataLoader.swift
+//  Astronomy
+//
+//  Created by Ilgar Ilyasov on 3/4/19.
+//  Copyright © 2019 Lambda School. All rights reserved.
+//
+
+import Foundation
+
+protocol NetworkDataLoader {
+    
+    func loadData(from request: URLRequest, completion: @escaping (Data?, URLResponse?, Error?) -> Void)
+    func loadData(from url: URL, completion: @escaping (Data?, URLResponse?, Error?) -> Void)
+}
+
+extension URLSession: NetworkDataLoader {
+    
+    func loadData(from request: URLRequest, completion: @escaping (Data?, URLResponse?, Error?) -> Void) {
+        dataTask(with: request, completionHandler: completion).resume()
+    }
+    
+    func loadData(from url: URL, completion: @escaping (Data?, URLResponse?, Error?) -> Void) {
+        dataTask(with: url, completionHandler: completion).resume()
+    }
+    
+}
